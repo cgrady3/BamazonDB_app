16a2ce7ad456326be1ec1cdddc06d4e28b8c2f43
Carolyn Grady <owenv8@gmail.com>
2020-03-09 18:57:57 -0400

    bamazon cutomer complete, not to debug

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
+// create the connection information for the sql database
 var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
@@ -9,35 +10,76 @@ var connection = mysql.createConnection({
   database: 'bamazon'
 });
 
+
+// connect to the mysql server and sql database
 connection.connect(function (err) {
   if (err) throw err;
-  console.log('connected as id ' + connection.threadId);
-  start();
+  // run the start function after the connection is made to prompt the user
+  showItems();
 });
 
-function start() {
-  inquirer
-    .prompt({
-      name: 'item',
-      type: 'list',
-      message: 'Would you like to buy?',
-      choices: function () {
-        var choiceArray = [];
-        for (var i = 0; i < results.length; i++) {
-          choiceArray.push(results[i].item_name);
+function showItems() {
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log("Product: " + res[i].product_name + "\n" +
+        "Department: " + res[i].department_name + "\n" +
+        "Price: $" + res[i].price + "\n" +
+        "Stock: " + res[i].stock_quantity);
+      console.log("----------------------------------------");
+    }
+    buyItem();
+  });
+
+};
+
+function buyItem() {
+  inquirer.prompt([{
+    name: 'item',
+    type: 'list',
+    choices: function () {
+      var choiceArray = [];
+      for (var i = 0; i < res.length; i++) {
+        choiceArray.push(res[i].product_name);
+      }
+      return choiceArray;
+    },
+    message: 'What would you like to buy?',
+  }, {
+    name: "quantity",
+    message: "How many would you like to purchase?"
+  }]).then(function (answer) {
+
+    connection.query("SELECT * FROM products", function (err, res) {
+      if (err) throw err;
+
+      var chosenItem;
+      for (var i = 0; i < res.length; i++) {
+        if (res[i].item_id === answer.item) {
+          chosenItem = res[i];
         }
-        return choiceArray;
       }
-    })
-    .then(function (answer) {
-      // based on their answer, either call the bid or the post functions
-      if (answer.postOrBid === 'POST') {
-        postAuction();
+
+      if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
+        connection.query(
+          "UPDATE products SET ? WHERE ?",
+          [
+            {
+              stock_quantity: (chosenItem.stock_quantity - parseInt(answer.quantity))
+            },
+            {
+              item_id: chosenItem.item_id
+            }
+          ],
+          function (error) {
+            if (error) throw error;
+            console.log("Thank you for your business! Your total is " + "$" + parseInt(answer.quantity) * chosenItem.price);
+          }
+        );
       }
-      else if (answer.postOrBid === 'BID') {
-        bidAuction();
-      } else {
-        connection.end();
+      else {
+        console.log("We're sorry. We don't have enough in stock.");
       }
     });
-}
+  });
+};
